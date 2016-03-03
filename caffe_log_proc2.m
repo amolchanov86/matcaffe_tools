16837cea9955259db01bc89ca622604570988d37
artem <a.molchanov86@gmail.com>
2016-03-03 13:51:30 -0800

    training using shell output added

---
+++ caffe_log_proc2.m
@@ -0,0 +1,111 @@
+function [ train_res, test_res ] = caffe_log_proc2( log_filename )
+%% Description
+% processing log files produced by caffe in the shell
+% [ train_res, test_res ] = caffe_log_proc( log_filename )
+
+%% Parameters
+lines_prealloc = int32(1000000);
+iter_keyword = 'Iteration';
+testing_keyword = 'Testing';
+loss_keyword='loss';
+accuracy_keyword='accuracy';
+lr_keyword = 'lr';
+
+%% Execution
+%---Read all lines
+fid = fopen(log_filename);
+lines{lines_prealloc} = '';
+line_i = 1;
+lines{line_i} = fgets(fid);
+while ischar( lines{line_i} )
+    line_i = line_i + 1;
+    lines{line_i} = fgets(fid);
+end
+lines_num = line_i - 1;
+lines = lines(1:lines_num);
+%cause the last one is not a character
+%using normal braces to get subarray and not values
+
+fclose(fid);
+
+%---Line analisis
+%Preallocate cell arrays
+train_res{lines_num} = 0;
+test_res{lines_num} = 0;
+
+line_i = 1;
+train_i = 1;
+test_i = 1;
+
+train_res{train_i}.iter = 0;
+test_res{test_i}.iter = 0;
+
+
+% fprintf('%s : lines_num = %d ...\n', mfilename, lines_num);
+
+while line_i < lines_num
+    words = strsplit(lines{line_i}, ' ');
+    kw_pos = get_word_pos(words, {iter_keyword, testing_keyword, loss_keyword, lr_keyword} );
+   
+    if kw_pos{1}(1) > 0
+        iter_val_str = words{kw_pos{1}(1) + 1};
+        iter_val_str = strrep(iter_val_str, ',' , '');
+        iter_val = str2num(iter_val_str);
+        if kw_pos{2}(1) > 0
+            
+            if test_res{test_i}.iter < iter_val && test_res{test_i}.iter > 0
+                test_i = test_i+1;
+            end
+            
+            test_res{test_i}.iter = iter_val;
+            
+            % Exctract accuracy and loss values
+%             words = strsplit(lines{line_i+1}, ' ');
+%             accuracy_pos = get_word_pos(words, {accuracy_keyword}); %accuracy should be on the next line
+%             accuracy_val = str2num(words{accuracy_pos{1}(1) + 2});
+            accuracy_val = -1;
+
+            words = strsplit(lines{line_i+1}, ' ');
+            loss_pos = get_word_pos(words, {loss_keyword}); %loss should be on the 2nd line
+            loss_val = str2num(words{loss_pos{1}(1) + 2});
+%             fprintf('%s : loss_test_val = %f \n', mfilename, loss_val);
+            
+            test_res{test_i}.loss = loss_val;
+            test_res{test_i}.accuracy = accuracy_val;
+%             fprintf('%s : accuracy_test_val = %f \n', mfilename, accuracy_val);
+            
+            line_i = line_i+1; %Skipping the next 1 lines
+            
+        else % These are training results since testing words was not found
+            if train_res{train_i}.iter < iter_val && train_res{train_i}.iter > 0
+                train_i = train_i+1;
+            end
+            
+            % Exctract loss value
+            train_res{train_i}.iter = iter_val;
+            
+            if kw_pos{3}(1) > 0 %loss
+                loss_pos =  kw_pos{3}(1) + 2;
+                loss_val = str2num(words{loss_pos});
+                train_res{train_i}.loss = loss_val;
+%                 fprintf('%s : loss_val = %f \n', mfilename, loss_val);
+            elseif kw_pos{4}(1) > 0 %lr
+                lr_pos = kw_pos{4}(1) + 2;
+                lr_val = str2num(words{lr_pos});
+                train_res{train_i}.lr = lr_val;
+%                 fprintf('%s : lr_val = %f \n', mfilename, lr_val);
+            end
+        end
+        
+    end
+    line_i = line_i+1;
+    
+end
+
+train_res = train_res(1:train_i);
+test_res = test_res(1:test_i);
+
+fprintf('%s : file_name = %s , lines_num = %d , train_iter_num = %d , test_iter_num = %d \n', mfilename, log_filename, lines_num, length(train_res), length(test_res) );
+
+end
+
---
+++ caffe_trainloss_net_shell.m
@@ -0,0 +1,140 @@
+function [ best_loss, best_iter, stat ] = caffe_trainloss_net_shell( solver_filename, best_snapshot_prefix, varargin )
+%% Description:
+% training with saving best snapshot using shell (since I have problems
+% with matcaffe reporting wrong accuracy)
+% WARNING: for now, make sure that testing and saving of snapshots happens
+% with the same interval
+% [ best_loss, best_iter, stat ] = caffe_train_net_shell( solver_filename, best_snapshot_prefix, [log_filename], [clear_other_snapshots = 1] )
+% --- INPUT:
+% solver_filename = solver to use
+% best_snapshot_prefix = all temporary snapshots will be saved with the
+%   prefix reported in solver protobuf file, but at the end they all be
+%   erased except the one with the best loss, which will be moved
+%   (renamed) according to the best_snapshot_prefix
+% --- OPTIONAL
+% log_filename = log with training losses/accuracies reported
+% clear_other_snapshots = deletes non best snapshots at the end (warning:
+%   be careful. Make sure that the folder where you are saving temporary
+%   snapshots does not contain important snapshots)
+% --- OUTPUT
+% best_loss = best loss
+% best_iter = best iteration
+% stat = contains losses reported during training (from parsing the log
+% file)
+
+%% Parameters
+
+
+%% Arguments
+var_i = 1;
+[snap_p] = fileparts(best_snapshot_prefix);
+log_filename = [ snap_p '/training_log.txt' ];
+if length(varargin) >= var_i
+    log_filename = varargin{var_i};
+else
+    if ~exist(snap_p, 'dir')
+        fprintf('WARNING: %s : Dir = %s didnt exist, so it was created ...\n', ...
+            mfilename, ...
+            snap_p);
+        mkdir(snap_p);
+    end
+end
+fprintf('%s : log filename = %s \n', mfilename, log_filename);
+
+var_i = 2;
+clear_other_snapshots = 1;
+if length(varargin) >= var_i
+    clear_other_snapshots = varargin{var_i};
+end
+
+%% Execution
+solver_params = caffe_read_solverprototxt(solver_filename);
+% solver_params.snapshot_prefix;
+
+% --- Run training
+system(['caffe train --solver=' solver_filename ' 2>' log_filename]);
+
+% --- Analyze log file
+% [stat.train, test_stat] = caffe_log_proc(log_filename);
+[stat.train, test_stat] = caffe_log_proc2(log_filename);
+
+% --- Pick best iteration
+best_iter_i = 0;
+best_iter = 0;
+best_loss = 99999999999999999999999999999999;
+for i=1:length(test_stat)
+    if (test_stat{i}.loss < best_loss) || (best_iter == 0)
+        best_loss =  test_stat{i}.loss;
+        best_iter_i = i;
+        best_iter = test_stat{i}.iter;
+    end
+end
+
+model_ext = '.caffemodel';
+solver_ext = '.solverstate';
+
+best_snapshot_temp_name = sprintf('%s_iter_%d', ...
+    solver_params.snapshot_prefix, ...
+    best_iter );
+best_snapshot_name = sprintf('%s__iter_%06d__loss_%5.10f', ...
+    best_snapshot_prefix, ...
+    best_iter, ...
+    best_loss );
+
+stat.best_snapshot_name = [best_snapshot_name model_ext];
+[best_snapshot_path best_snapshot_name_only best_snapshot_ext_only] = fileparts(best_snapshot_name);
+best_snapshot_name_only = [best_snapshot_name_only best_snapshot_ext_only];
+%because sometimes snapshot prefix contains '.' and fileparts thinks it is
+%an extension
+
+% --- Creating folders if they don't exist
+snap_p = fileparts(best_snapshot_name);
+
+if ~exist(snap_p, 'dir')
+    fprintf('WARNING: %s : Dir = %s didnt exist, so it was created ...\n', ...
+        mfilename, ...
+        snap_p);
+    mkdir(snap_p);
+end
+
+% --- Rename files with the best iteration
+fprintf('%s : renaming %s to %s \n', mfilename, [best_snapshot_temp_name model_ext], stat.best_snapshot_name);
+movefile( [best_snapshot_temp_name model_ext], ...
+    stat.best_snapshot_name );
+
+fprintf('%s : renaming %s to %s \n', mfilename, [best_snapshot_temp_name solver_ext], [best_snapshot_name solver_ext]);
+movefile( [best_snapshot_temp_name solver_ext] , ...
+    [best_snapshot_name solver_ext]);
+
+% --- Delete non best snapshots
+if clear_other_snapshots
+%     system( ['rm -rf ' solver_params.snapshot_prefix '*' model_ext ] );
+%     system( ['rm -rf ' solver_params.snapshot_prefix '*' solver_ext ] );
+    delete_except( [solver_params.snapshot_prefix '*' model_ext],  [ best_snapshot_name_only model_ext] );
+    delete_except( [solver_params.snapshot_prefix '*' solver_ext], [ best_snapshot_name_only solver_ext] );
+end
+
+% --- Copy data to stat
+iterations_num = length(test_stat);
+stat.iterations = zeros(1, iterations_num);
+stat.loss = zeros(1, iterations_num);
+stat.loss_train_sync = zeros(1, iterations_num);
+
+stat.loss_train = zeros(1 , length(length(stat.train)) );
+stat.iterations_train = zeros(1 , length(length(stat.train)) );
+
+for iter_i=1:length(stat.train)
+    stat.loss_train(iter_i) = stat.train{iter_i}.loss;
+    stat.iterations_train(iter_i) = stat.train{iter_i}.iter;
+end
+
+for iter_i=1:iterations_num
+    stat.iterations(iter_i) = test_stat{iter_i}.iter;
+    stat.loss_test(iter_i) = test_stat{iter_i}.loss;
+    stat.loss_train_sync(iter_i) = ...
+        stat.loss_train( stat.iterations_train == stat.iterations(iter_i) );
+end
+stat.loss = stat.loss_test;
+
+end
+
